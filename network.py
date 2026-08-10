729094374b9f6f9189d4e8c926fcedcb7e9f082a
fajersaleh <fajersaleh39@gmail.com>
2026-08-10 22:11:51 +0300

    Update network.py

--- network.py
+++ network.py
@@ -1,33 +1,51 @@
-#!/bin/python
+#!/usr/bin/env python3
+
 import subprocess
+import socket
+
+# Network Discovery
 
 def discover_hosts():
+    print("Discovering devices...", flush=True)
 
     for i in range(1, 20):
         ip = "10.0.2." + str(i)
 
+        print(f"Checking {ip}...", flush=True)
+
         result = subprocess.run(
-            ["ping", "-c", "1", ip],
+            ["ping", "-c", "1", "-W", "1", ip],
             capture_output=True,
             text=True
         )
 
         if result.returncode == 0:
-            print(ip, "is Active")
+            print(ip, "is Active", flush=True)
+
 
 discover_hosts()
 
-import socket
-target = input("Enter Target IP")
-ports = [20,21,22,23,25,53,80,443,8080]
+# Port Scanner
+
+print("\nPORT SCANNER", flush=True)
+
+target = input("Enter Target IP: ")
+
+ports = [20, 21, 22, 23, 25, 53, 80, 443, 8080]
+
+print(f"\nScanning target: {target}")
+
 for port in ports:
-    s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
-    
-    result= s.connect_ex((target,port))
+
+    result = s.connect_ex((target, port))
+
     if result == 0:
-       print(f"Port{port}:OPEN")
+        print(f"Port {port}: OPEN")
     else:
-       print(f"Port{port}:CLOSED")
+        print(f"Port {port}: CLOSED")
+
     s.close()
 
+print("Port scan completed.")
