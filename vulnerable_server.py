281c081e96a21abe6f6a47482cc50b51add7f7a7
fajersaleh <fajersaleh39@gmail.com>
2026-08-11 06:56:19 +0300

    Update vulnerable_server.py

--- vulnerable_server.py
+++ vulnerable_server.py
@@ -1,34 +1,7 @@
+
 #!/bin/python
 
 import socket
-
-HOST = "0.0.0.0"
-PORT = 8080
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server.bind((HOST, PORT))
-server.listen(5)
-
-print(f"Lab test server is running on port {PORT}")
-
-while True:
-    client, address = server.accept()
-
-    print("Connection from:", address)
-
-    message = """Welcome to the Cybersecurity Lab Test Server.
-
-This server is used for testing the network port scanner.
-"""
-
-    client.send(message.encode())
-    client.close()
-#!/bin/python
-
-import socket
-# Lab Test Server Configuration
 
 HOST = "0.0.0.0"
 PORT = 8080
